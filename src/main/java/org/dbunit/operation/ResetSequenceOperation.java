cd27869394fe78b7e538c9e5afdb096d566dd5ad
cheese8 <yinwerhong@163.com>
2024-05-05 10:11:58 +0800

    add databaseType

---
+++ src/main/java/org/dbunit/operation/ResetSequenceOperation.java
@@ -0,0 +1,25 @@
+package org.dbunit.operation;
+
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ResetSequenceOperation extends DatabaseOperation {
+
+    @Override
+    public void execute(IDatabaseConnection connection, IDataSet dataSet) throws DatabaseUnitException, SQLException {
+        String[] tables = dataSet.getTableNames();
+        DatabaseConfig databaseConfig = connection.getConfig();
+        String databaseType = String.valueOf(databaseConfig.getProperty(DatabaseConfig.PROPERTY_DATABASE_TYPE));
+        Statement statement = connection.getConnection().createStatement();
+        for (String table : tables) {
+            int startWith = dataSet.getTable(table).getRowCount() + 1;
+            statement.execute("alter sequence " + table + "_PK_SEQ RESTART WITH "+ startWith);
+
+        }
+    }
+}
